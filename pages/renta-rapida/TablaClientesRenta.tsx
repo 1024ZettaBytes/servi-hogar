7d51f488984da1ff9413324e94cf64b8798f5cad
Eduardo Ramirez <ramirezcota.eduardo@gmail.com>
2022-09-22 22:37:55 -0600

    Add: Renta Rapida step 1 (missing change residence)

---
+++ pages/renta-rapida/TablaClientesRenta.tsx
@@ -0,0 +1,410 @@
+import { FC, ChangeEvent, useState } from "react";
+import * as str from "string";
+import PropTypes from "prop-types";
+import {
+  Divider,
+  Box,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableContainer,
+  Typography,
+  useTheme,
+  CardHeader,
+  TextField,
+  InputAdornment,
+  Button,
+  Grid,
+} from "@mui/material";
+import EditTwoToneIcon from "@mui/icons-material/EditTwoTone";
+
+import { deleteCustomers } from "../../lib/client/customersFetch";
+import { useSnackbar } from "notistack";
+import Label from "@/components/Label";
+import { CustomerLevel } from "@/models/crypto_order";
+import SearchIcon from "@mui/icons-material/Search";
+import GenericModal from "@/components/GenericModal";
+
+interface TablaClientesRentaProps {
+  className?: string;
+  customerList: any[];
+  selectedCustomer?: any;
+  onSelectCustomer: Function;
+}
+
+const getStatusLabel = (customerLevel: CustomerLevel): JSX.Element => {
+  const map = {
+    nuevo: {
+      text: "Nuevo",
+      color: "secondary",
+    },
+    regular: {
+      text: "Regular",
+      color: "info",
+    },
+    permanente: {
+      text: "Permanente",
+      color: "success",
+    },
+    deudor: {
+      text: "Deudor",
+      color: "warning",
+    },
+    conflictivo: {
+      text: "Conflictivo",
+      color: "error",
+    },
+  };
+
+  const { text, color }: any = map[customerLevel];
+
+  return <Label color={color}>{text}</Label>;
+};
+const compareStringsForFilter = (keyWord: string, field: string) => {
+  return str(field)
+    .latinise()
+    .toLowerCase()
+    .includes(str(keyWord).latinise().toLowerCase());
+};
+const applyFilters = (customerList: any[], filter: string): any[] => {
+  return customerList.filter((customer) => {
+    if (!filter || filter === "") {
+      return true;
+    }
+    return (
+      Object.entries(customer).filter((keyValue) => {
+        const key = keyValue[0];
+        const value = keyValue[1];
+        if (!value) {
+          return false;
+        }
+        switch (key) {
+          case "currentResidence": {
+            const matchCity =
+              value["city"]?.name &&
+              compareStringsForFilter(filter, value["city"].name);
+            const matchSector =
+              value["sector"]?.name &&
+              compareStringsForFilter(filter, value["sector"].name);
+            const matchStreet =
+              value["street"] &&
+              compareStringsForFilter(filter, value["street"]);
+            const matchSuburb =
+              value["suburb"] &&
+              compareStringsForFilter(filter, value["suburb"]);
+            return matchCity || matchSector || matchStreet || matchSuburb;
+          }
+          case "level": {
+            const matchLevel =
+              value["name"] && compareStringsForFilter(filter, value["name"]);
+            return matchLevel;
+          }
+          case "name":
+          case "cell": {
+            return compareStringsForFilter(filter, value.toString());
+          }
+        }
+      }).length > 0
+    );
+  });
+};
+
+const applyPagination = (
+  customerList: any[],
+  page: number,
+  limit: number
+): any[] => {
+  return customerList.slice(page * limit, page * limit + limit);
+};
+
+const TablaClientesRenta: FC<TablaClientesRentaProps> = ({
+  customerList,
+  selectedCustomer,
+  onSelectCustomer,
+}) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [customersToDelete, setCustomersToDelete] = useState<string[]>([]);
+  const [selectedCustomers, setSelectedCustomers] = useState<string[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [limit, setLimit] = useState<number>(10);
+  const [filter, setFilter] = useState<string>("");
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const value = e.target.value;
+    setFilter(value);
+  };
+
+  const handlePageChange = (_event: any, newPage: number): void => {
+    setPage(newPage);
+  };
+
+  const handleLimitChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setLimit(parseInt(event.target.value));
+  };
+
+  const handleOnConfirmDelete = async () => {
+    setIsDeleting(true);
+    const result = await deleteCustomers(customersToDelete);
+    setDeleteModalIsOpen(false);
+    setIsDeleting(false);
+    enqueueSnackbar(result.msg, {
+      variant: !result.error ? "success" : "error",
+      anchorOrigin: {
+        vertical: "top",
+        horizontal: "center",
+      },
+      autoHideDuration: 2000,
+    });
+
+    if (!result.error) {
+      const newSelected = selectedCustomers.filter(
+        (selected) => !customersToDelete.includes(selected)
+      );
+      setSelectedCustomers(newSelected);
+      setCustomersToDelete([]);
+    }
+  };
+
+  const filteredCostumers = applyFilters(customerList, filter);
+  const paginatedCustomers = applyPagination(filteredCostumers, page, limit);
+  const theme = useTheme();
+  return (
+    <>
+      <Card>
+        <CardHeader
+          action={
+            <Box width={200}>
+              <>
+                <TextField
+                  size="small"
+                  id="input-search-customer"
+                  label="Buscar"
+                  onChange={handleSearchChange}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                  sx={{ marginTop: "20px" }}
+                />
+              </>
+            </Box>
+          }
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+          }}
+          title=""
+        />
+        <Divider />
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Cliente</TableCell>
+                <TableCell align="center">Celular</TableCell>
+                <TableCell align="center">Domicilio</TableCell>
+                <TableCell align="center">Ciudad</TableCell>
+                <TableCell align="center">Nivel</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {paginatedCustomers.map((customer) => {
+                const isSelected = selectedCustomer?._id === customer?._id;
+                return (
+                  <TableRow
+                    key={customer?._id}
+                    selected={isSelected}
+                    sx={{
+                      "&:hover": {
+                        backgroundColor: isSelected
+                          ? theme.colors.primary
+                          : "transparent",
+                      },
+                      cursor: "pointer",
+                    }}
+                    onClick={() => {
+                      onSelectCustomer(customer?._id);
+                    }}
+                  >
+                    <TableCell align="center">
+                      <Typography
+                        variant="body1"
+                        fontWeight="bold"
+                        color="text.primary"
+                        gutterBottom
+                        noWrap
+                      >
+                        {customer?.name}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Typography
+                        variant="body1"
+                        fontWeight="bold"
+                        color="text.primary"
+                        gutterBottom
+                        noWrap
+                      >
+                        {customer?.cell}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Typography
+                        variant="body1"
+                        fontWeight="bold"
+                        color="text.primary"
+                        gutterBottom
+                        noWrap
+                      >
+                        {customer?.currentResidence?.street}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary" noWrap>
+                        {customer?.currentResidence?.suburb}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Typography
+                        variant="body1"
+                        fontWeight="bold"
+                        color="text.primary"
+                        gutterBottom
+                        noWrap
+                      >
+                        {customer?.currentResidence?.city?.name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary" noWrap>
+                        {customer?.currentResidence?.sector?.name}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="center">
+                      {getStatusLabel(customer?.level?.id)}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box p={2}>
+          <TablePagination
+            component="div"
+            count={filteredCostumers.length}
+            onPageChange={handlePageChange}
+            onRowsPerPageChange={handleLimitChange}
+            page={page}
+            rowsPerPage={limit}
+            rowsPerPageOptions={[5, 10, 25, 30]}
+          />
+        </Box>
+      </Card>
+      { selectedCustomer &&
+      <Grid container marginTop={1}>
+        <Grid item sm={12} md={12} lg={6}>
+          <Card>
+            <CardHeader
+              title="Domicilio del cliente"
+              action={
+                <Box>
+                  <Button
+                    startIcon={<EditTwoToneIcon />}
+                    size="medium"
+                    variant="text"
+                    sx={{ marginTop: 1 }}
+                  >
+                    Modificar
+                  </Button>
+                </Box>
+              }
+            />
+            <Divider />
+            <Grid container p={1}>
+              <Grid item sm={3} lg={4} margin={1}>
+                <Typography variant="h5">Celular</Typography>
+                <Typography variant="h5" sx={{ py: 1 }} fontWeight="normal">
+                  {selectedCustomer?.cell}
+                </Typography>
+              </Grid>
+              <Grid item sm={4} lg={7} margin={1}>
+                <Typography variant="h5">Calle y Número</Typography>
+                <Typography variant="h5" sx={{ py: 1 }} fontWeight="normal">
+                  {selectedCustomer?.currentResidence?.street}
+                </Typography>
+              </Grid>
+              <Grid item sm={4} lg={4} margin={1}>
+                <Typography variant="h5">Colonia</Typography>
+                <Typography variant="h5" sx={{ py: 1 }} fontWeight="normal">
+                  {selectedCustomer?.currentResidence?.suburb}
+                </Typography>
+              </Grid>
+              <Grid item sm={3} lg={7} margin={1}>
+                <Typography variant="h5">Sector</Typography>
+                <Typography variant="h5" sx={{ py: 1 }} fontWeight="normal">
+                  {selectedCustomer?.currentResidence?.sector?.name}
+                </Typography>
+              </Grid>
+              <Grid item sm={4} lg={4} margin={1}>
+                <Typography variant="h5">Referencia domicilio</Typography>
+                <Typography variant="h5" sx={{ py: 1 }} fontWeight="normal">
+                  {selectedCustomer?.currentResidence?.residenceRef}
+                </Typography>
+              </Grid>
+              <Grid item sm={4} lg={4} margin={1}>
+                <Typography variant="h5">Nombre referencia</Typography>
+                <Typography variant="h5" sx={{ py: 1 }} fontWeight="normal">
+                  {selectedCustomer?.currentResidence?.nameRef}
+                </Typography>
+              </Grid>
+              <Grid item sm={4} lg={2} margin={1}>
+                <Typography variant="h5">Teléfono ref.</Typography>
+                <Typography variant="h5" sx={{ py: 1 }} fontWeight="normal">
+                  {selectedCustomer?.currentResidence?.telRef}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+      </Grid>
+}
+      <GenericModal
+        open={deleteModalIsOpen}
+        title="Atención"
+        text={
+          "¿Esta seguro de eliminar a" +
+          (customersToDelete.length === 1
+            ? "l cliente seleccionado"
+            : " los clientes seleccionados") +
+          "?"
+        }
+        isLoading={isDeleting}
+        onAccept={handleOnConfirmDelete}
+        onCancel={() => {
+          setDeleteModalIsOpen(false);
+          setIsDeleting(false);
+        }}
+      />
+    </>
+  );
+};
+
+TablaClientesRenta.propTypes = {
+  customerList: PropTypes.array.isRequired,
+  selectedCustomer: PropTypes.object,
+  onSelectCustomer: PropTypes.func.isRequired,
+};
+
+TablaClientesRenta.defaultProps = {
+  selectedCustomer: {},
+  customerList: [],
+};
+
+export default TablaClientesRenta;
---
+++ pages/renta-rapida/index.tsx
@@ -0,0 +1,230 @@
+import Head from "next/head";
+import { getSession } from "next-auth/react";
+import { useState } from "react";
+import SidebarLayout from "@/layouts/SidebarLayout";
+import { validateServerSideSession } from "../../lib/auth";
+import PageHeader from "@/components/PageHeader";
+import PageTitleWrapper from "@/components/PageTitleWrapper";
+import { Card, Container, Grid, Skeleton, Alert, Box } from "@mui/material";
+import Footer from "@/components/Footer";
+import AddCustomerModal from "@/components/AddCustomerModal";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepContent from "@mui/material/StepContent";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import AddIcon from "@mui/icons-material/Add";
+import {
+  useGetAllCustomers,
+  getFetcher,
+  useGetCities,
+} from "../api/useRequest";
+import { useSnackbar } from "notistack";
+
+import NextBreadcrumbs from "@/components/Shared/BreadCrums";
+import TablaClientesRenta from "./TablaClientesRenta";
+
+function RentaRapida() {
+  const paths = ["Inicio", "Renta Rápida"];
+  const { enqueueSnackbar } = useSnackbar();
+  const { customerList, customerError } = useGetAllCustomers(getFetcher);
+  const { citiesList, citiesError } = useGetCities(getFetcher);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedId, setSelectedId] = useState<any>(null);
+  const generalError = customerError || citiesError;
+  const completeData = customerList && citiesList;
+  const steps = [
+    {
+      label: "Seleccione un cliente",
+    },
+    {
+      label: "Tiempo de renta",
+      description:
+        "An ad group contains one or more ads which target a shared set of keywords.",
+    },
+    {
+      label: "Entrega",
+      description: `Try out different ad text to see what brings in the most customers,
+                and learn how to enhance your ads using features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`,
+    },
+  ];
+  const handleClickOpen = () => {
+    setModalIsOpen(true);
+  };
+const getSelectedCustomer = (id, cList) =>{
+  return cList.find(c => c._id.toString() === id);
+}
+const selectedCustomer = getSelectedCustomer(selectedId, customerList ? customerList : []) || null;
+console.log("Selected customer", selectedCustomer);
+  const handleClose = (addedCustomer, successMessage = null) => {
+    setModalIsOpen(false);
+    if (addedCustomer && successMessage) {
+      enqueueSnackbar(successMessage, {
+        variant: "success",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "center",
+        },
+        autoHideDuration: 1500,
+      });
+    }
+  };
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+  return (
+    <>
+      <Head>
+        <title>Clientes</title>
+      </Head>
+      <PageTitleWrapper>
+        <PageHeader title={"Nueva Renta"} sutitle={""} />
+        <NextBreadcrumbs paths={paths} lastLoaded={true} />
+      </PageTitleWrapper>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="stretch"
+          spacing={4}
+        >
+          <Grid item xs={12}>
+            {generalError ? (
+              <Alert severity="error">
+                {customerError?.message || citiesError?.message}
+              </Alert>
+            ) : !completeData ? (
+              <Skeleton
+                variant="rectangular"
+                width={"100%"}
+                height={500}
+                animation="wave"
+              />
+            ) : (
+              <Card sx={{ p: 2 }}>
+                <Stepper
+                  activeStep={activeStep}
+                  orientation="vertical"
+                  sx={{ backgroundColor: "" }}
+                >
+                  {steps.map((step, index) => (
+                    <Step key={step.label}>
+                      <StepLabel
+                        optional={
+                          index === 2 ? (
+                            <Typography variant="caption">
+                              Último paso
+                            </Typography>
+                          ) : null
+                        }
+                      >
+                        {step.label}
+                      </StepLabel>
+                      <StepContent>
+                        {index === 0 && (
+                          <>
+                            <Grid container >
+                              <Grid item xs={12} md={4} lg={4}></Grid>
+                              <Grid item xs={12} md={4} lg={4}>
+                                <Box flex={1} p={2}>
+                                  <Button
+                                    fullWidth
+                                    startIcon={<AddIcon />}
+                                    size="medium"
+                                    variant="contained"
+                                    sx={{ marginTop: 1 }}
+                                    onClick={handleClickOpen}
+                                  >
+                                    Nuevo
+                                  </Button>
+                                </Box>
+                              </Grid>
+                              <Grid item xs={12} md={4} lg={4}></Grid>
+                              <Grid item xs={12} md={12} lg={12}>
+                              <TablaClientesRenta
+                              customerList={customerList}
+                              selectedCustomer={selectedCustomer}
+                              onSelectCustomer={setSelectedId}
+                            />
+                              </Grid>
+                            </Grid>
+                            
+                          </>
+                        )}
+                        <Box sx={{ mb: 2 }}>
+                          <div>
+                            <Button
+                              disabled={!selectedCustomer}
+                              variant="contained"
+                              onClick={handleNext}
+                              sx={{ mt: 1, mr: 1 }}
+                            >
+                              {index === steps.length - 1
+                                ? "Rentar"
+                                : "Siguiente"}
+                            </Button>
+                            {index > 0 && (
+                              <Button
+                                onClick={handleBack}
+                                sx={{ mt: 1, mr: 1 }}
+                              >
+                                Back
+                              </Button>
+                            )}
+                          </div>
+                        </Box>
+                      </StepContent>
+                    </Step>
+                  ))}
+                </Stepper>
+
+                {activeStep === steps.length && (
+                  <Paper square elevation={0} sx={{ p: 3 }}>
+                    <Typography>
+                      All steps completed - you&apos;re finished
+                    </Typography>
+                    <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+                      Reset
+                    </Button>
+                  </Paper>
+                )}
+              </Card>
+            )}
+          </Grid>
+        </Grid>
+      </Container>
+      {modalIsOpen && completeData ? (
+        <AddCustomerModal
+          open={modalIsOpen}
+          handleOnClose={handleClose}
+          citiesList={citiesList}
+          customerList={customerList}
+        />
+      ) : null}
+      <Footer />
+    </>
+  );
+}
+
+RentaRapida.getLayout = (page) => <SidebarLayout>{page}</SidebarLayout>;
+
+export async function getServerSideProps({ req, resolvedUrl }) {
+  let props = await validateServerSideSession(getSession, req, resolvedUrl);
+  return props;
+}
+export default RentaRapida;
