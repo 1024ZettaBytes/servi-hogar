96fa82c66c1343650c8d9a9d5c6ddb0d845c9267
Eduardo Ramírez <ramirezcota.eduardo@gmail.com>
2022-11-03 02:24:12 -0700

    Removed unused imports and vars xd

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,17 +1,11 @@
 import {
   Box,
-  Card,
   Typography,
   Container,
-  Divider,
   Button,
-  FormControl,
-  OutlinedInput,
-  InputAdornment,
   styled
 } from '@mui/material';
 import Head from 'next/head';
-import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
 import type { ReactElement } from 'react';
 import BaseLayout from 'src/layouts/BaseLayout';
 
@@ -35,17 +29,7 @@ const TopWrapper = styled(Box)(
 `
 );
 
-const OutlinedInputWrapper = styled(OutlinedInput)(
-  ({ theme }) => `
-    background-color: ${theme.colors.alpha.white[100]};
-`
-);
 
-const ButtonSearch = styled(Button)(
-  ({ theme }) => `
-    margin-right: -${theme.spacing(1)};
-`
-);
 
 function Status404() {
   return (
