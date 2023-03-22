6ab1958db581029e822821df50981029287d6e40
Eduardo Ramirez <ramirezcota.eduardo@gmail.com>
2023-03-22 11:26:54 -0700

    Add: machine on customer movements

--- lib/models/CustomerMovement.ts
+++ lib/models/CustomerMovement.ts
@@ -3,6 +3,7 @@ import mongoose, { Model, model, Schema } from 'mongoose';
 export interface ICustomerMovement extends Document {
   customer: Schema.Types.ObjectId;
   rent: Schema.Types.ObjectId;
+  machine: Schema.Types.ObjectId;
   type: string;
   description: string;
   date: Date;
@@ -11,6 +12,7 @@ export interface ICustomerMovement extends Document {
 const CustomerMovementSchema = new Schema<ICustomerMovement>({
   customer: { type: Schema.Types.ObjectId, required: true, ref:'customers' },
   rent: { type: Schema.Types.ObjectId, default: null, ref:'rents' },
+  machine: { type: Schema.Types.ObjectId, default: null, ref:'machines' },
   type: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, required: true }
