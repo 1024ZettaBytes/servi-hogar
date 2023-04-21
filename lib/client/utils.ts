bd139486a9d413c351558e9b4923a3b97fa66472
Eduardo Ramirez <ramirezcota.eduardo@gmail.com>
2023-04-21 10:51:22 -0700

    fix formattz?

--- lib/client/utils.ts
+++ lib/client/utils.ts
@@ -162,7 +162,7 @@ export const sleep = async (duration: number): Promise<void> => {
 };
 
 export const formatTZDate = (date: Date, format: string): string => {
-  return dayjs.utc(date).tz('America/Mexico_City').format(format);
+  return dayjs(date).utc(true).tz('America/Mexico_City').format(format);
 };
 
 export const convertDateToTZ = (date: Date): Date => {
