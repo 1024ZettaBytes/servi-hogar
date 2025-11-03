5e6e44522a9aa65cfe5b3b61c1925845b57592dd
Eduardo Ramirez <ramirezcota.eduardo@gmail.com>
2025-11-03 12:15:08 -0700

    improved login

--- pages/auth.js
+++ pages/auth.js
@@ -7,18 +7,28 @@ import LoginForm from "./login";
 function AuthPage() {
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
+  
   useEffect(() => {
+    let isMounted = true;
+    
     getSession().then((session) => {
+      if (!isMounted) return;
+      
       if (session) {
         if (session.user.wasRemoved) {
           signOut();
         } else {
-          router.replace("/");
+          const returnUrl = router.query?.returnUrl || "/";
+          router.replace(returnUrl);
         }
       } else {
         setIsLoading(false);
       }
     });
+    
+    return () => {
+      isMounted = false;
+    };
   }, [router]);
 
   if (isLoading) {
